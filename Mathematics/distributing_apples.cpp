8628ebade6157f067ec7b822d495fc8747e0eb69
Kathan Vakharia <kathankv5201@gmail.com>
2025-11-19 19:36:58 +0530

    fix comments in distributing_apples.cpp for accuracy

--- Mathematics/distributing_apples.cpp
+++ Mathematics/distributing_apples.cpp
@@ -17,7 +17,7 @@ ll binpow(ll a, ll b, ll M) {
 }  //* T: O(log b)
 
 //- Global Variables
-const ll maxN = 2e6 + 10; //> Increased size to handle (a+b-1) up to 2e6
+const ll maxN = 2e6 + 10; //> Increased size to handle (n+m-1) up to 2e6
 ll M = 1e9 + 7;
 vector<ll> fact(maxN + 1, 1);
 
@@ -39,7 +39,7 @@ int main() {
     ll n, m;
     cin >> n >> m;
 
-    //> Result: C(n+m-1, b)
+    //> Result: C(n+m-1, m)
     cout << calc_nCr(n + m - 1, m) << "\n";
 }  //* T: O(n + log M) where n=O(1e6), M=1e9+7
    //* where O(1e6) for precomputing factorials, and O(log M) for modular inverse calculation
