356fa3f64fbb7e48152e3e33f64bc7a9c2462824
Kathan Vakharia <kathankv5201@gmail.com>
2025-12-20 20:56:57 +0530

    added codes for mtx exponentiations

---
+++ Mathematics/fibonacci_numbers.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int M = 1e9 + 7;
+
+//> Matrix Structure
+template <int N>
+struct Matrix {
+    ll m[N][N];
+
+    //> Identity Matrix
+    static Matrix<N> identity() {
+        Matrix<N> I{};
+        for (int i = 0; i < N; i++)
+            I.m[i][i] = 1;
+        return I;
+    }
+
+    //> Matrix Multiplication: overload * operator
+    Matrix<N> operator*(Matrix<N> b) {
+        Matrix<N> r{};
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                //* Calculate r[i][j] = ith row * jth col
+                for (int k = 0; k < N; k++) {
+                    r.m[i][j] += m[i][k] * b.m[k][j];
+                    r.m[i][j] %= M;
+                }
+            }
+        }
+        return r;
+    }
+
+    //> Matrix Exponentiation
+    Matrix<N> power(ll b) {
+        Matrix<N> r = identity();
+        auto a = *this;
+        while (b > 0) {
+            if (b & 1)
+                r = r * a;
+
+            a = a * a;
+            b >>= 1;
+        }
+        return r;
+    }
+};
+
+int main() {
+    ll n;
+    cin >> n;
+
+    Matrix<2> X{{{0, 1}, {1, 1}}};
+    cout << X.power(n).m[0][1] << "\n";
+}  //* T: O(8*log(n)), S: O(1)
